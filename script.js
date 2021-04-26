2ee9d7b8973f33f40f3413e427171b9e1e637b4c
Bunleap <bunleap.sorn@gmail.com>
2021-04-26 16:59:18 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ $.getJSON("https://corona.lmao.ninja/v3/covid-19/countries/cambodia", function (
     $("#active-case").append(data.active);
     $("#deaths").append(data.deaths);
     $("#recovered").append(data.recovered);
-    $("#today-case").append(data.todayCases);
-    $("#today-recover").append(data.todayRecovered);
+    $("#today-case").append("+" + data.todayCases);
+    $("#today-recover").append("+" + data.todayRecovered);
+    $("#population").append(Intl.NumberFormat().format(data.population));
+    $("#continent").append(data.continent);
+    $("#lat").append(data.countryInfo.lat);
+    $("#long").append(" " + data.countryInfo.long);
+
+    //mobile
+    $("#total-case-1").append(data.cases);
+    $("#active-case-1").append(data.active);
+    $("#deaths-1").append(data.deaths);
+    $("#recovered-1").append(data.recovered);
+    $("#today-case-1").append("+" + data.todayCases);
+    $("#today-recover-1").append("+" + data.todayRecovered);
+    $("#population-1").append(Intl.NumberFormat().format(data.population));
+    $("#continent-1").append(data.continent);
+    $("#lat-1").append(data.countryInfo.lat);
+    $("#long-1").append(" " + data.countryInfo.long);
 });
